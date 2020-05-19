1a57debf4dfcffb6a04d51b6ee4cc6f562674bf3
Stefan Urosevic <stefanuros@gmail.com>
2020-05-18 22:17:10 -0400

    implemented some store functionality

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,9 @@
-export const serverConfig = {
+export const serverConfig = Object.freeze({
   PORT: 8000,
   HOST: "localhost"
-};
+});
+
+export const storeConfig = Object.freeze({
+  MAX_TWEETS_STORED: 100,
+  MAX_TWEETS_REQUESTED: 20
+});
--- src/store.ts
+++ src/store.ts
@@ -1,24 +1,45 @@
+import { storeConfig } from './config.ts';
+
+interface Tweet {
+  created_at: string,
+  id_str: string,
+  [key: string]: any
+}
+
 // A store object that stores tweet info
 export class Store {
   // This is where tweets are stored
-  private static tweets: object[] = [];
+  private static tweets: Tweet[] = [];
+  private static mostRecentId: string | null = null;
+  private static readonly maxStored: number = storeConfig.MAX_TWEETS_STORED;
+  private static readonly maxRequested: number = storeConfig.MAX_TWEETS_REQUESTED;
 
   // This function gets tweets from the Twitter Developer Api and saves them. It
   // keeps a max number of tweets defined in the config. It only gets tweets that
   // are after 
   public static updateTweets(): void {
-    Store.tweets.push({tweet: 1});
+    Store.tweets.push({created_at: "", id_str: "", tweet: 1});
     console.log(Store.tweets);
   }
 
   // This function returns a list of the 20 most recent tweets
-  public static getTweets(): object[] {
-    return [];
+  public static getTweets(num: number = Store.maxRequested): Tweet[] {
+    return Store.tweets.slice(0, Math.min(num, Store.maxRequested));
   }
 
   // This function returns a list of the 20 most recent tweets that come after 
-  // 
-  public static getTweetsSince(): object[] {
-    return [];
+  // a specific tweet id
+  public static getTweetsSince(id: string): Tweet[] {
+    let c = 0;
+
+    while(id < Store.tweets[c].id_str) {
+      c++;
+
+      if(c === Store.maxRequested) {
+        return Store.getTweets();
+      }
+    }
+
+    return Store.getTweets(c);
   }
 }
