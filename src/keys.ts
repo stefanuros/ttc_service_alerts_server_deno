1bfe3d3f19c54e1529802d4ab9ebad2509ea00de
Stefan Urosevic <stefanuros@gmail.com>
2020-05-19 23:10:30 -0400

    Added gitignore and removed keys.ts

--- src/keys.ts
+++
@@ -1,6 +0,0 @@
-export const keys = {
-  consumerApiKey: "GttyRP88M5vqgGQIi7iW1sOuj",
-  consumerApiSecret: "Utn0Io20SX36nHZctHqL93136FJnNh1McxLoKZOu8OPiPYXt1J",
-  accessToken: "763464253089148928-GICdatpNUcxSl0SyHWgLwDe7YD6K28o",
-  accessTokenSecret: "BaWLNV6gvb8oNZVzJqCSpw13EYnXeCjVtysVIDsN6A3SV",
-}
